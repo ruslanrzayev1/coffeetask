c174006516a4fa359d15b902b00aa994d8925dc9
ruslanrzayev1 <sd7ga5a2n@code.edu.az>
2023-11-03 14:22:23 +0400

    coffee

---
+++ index.js
@@ -0,0 +1,10 @@
+// sessionStorage.setItem('1','SWP202')
+// sessionStorage.setItem('name','Aydan')
+// let arr = ['Ruslan','Camal','Cavidan','Cefer']
+// sessionStorage.setItem('2',JSON.stringify(arr))
+// let az = JSON.parse(sessionStorage.getItem('2'))
+// console.log(az);
+//  sessionStorage.clear()
+// let a = sessionStorage.getItem('1')
+// console.log(a);
+// sessionStorage.removeItem('1')
